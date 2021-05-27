7d6debfbe240ac6467d8f3232dd885e3aaa31e78
Paul Szenher <paul@szenher.com>
2021-05-27 17:24:19 -0400

    Refactor cloud_msgs into lego_loam ROS package, cleanup package.xml

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -1,27 +1,26 @@
 #ifndef _UTILITY_LIDAR_ODOMETRY_H_
 #define _UTILITY_LIDAR_ODOMETRY_H_
 
-
 #include <ros/ros.h>
 
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Odometry.h>
 
-#include "cloud_msgs/cloud_info.h"
+#include "lego_loam/cloud_info.h"
 
 #include <opencv/cv.h>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/range_image/range_image.h>
 #include <pcl/filters/filter.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/common/common.h>
 #include <pcl/registration/icp.h>
+
+#include <pcl_conversions/pcl_conversions.h>
 
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
